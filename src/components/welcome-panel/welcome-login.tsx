b373f365f0f80ec207f7ed3fc28d62beb6cf70d2
Evgeniya <74735099+sanny410@users.noreply.github.com>
2022-02-10 14:18:57 +0300

    feat: make an authorization page according to the layout

--- src/components/welcome-panel/welcome-login.tsx
+++ src/components/welcome-panel/welcome-login.tsx
@@ -1,8 +1,32 @@
 import React from "react";
+import {postData} from "../services";
+
+
 
 const WelcomeLogin = () => {
+    console.log(postData("https://api.jampad.ml/api/hrs/login", {
+        "email": "hr@gmail.com",
+        "password": "12345678qQ"
+    }));
     return (
-        <div>Логин</div>
+            <div className="authorization-form">
+                <form className="form-login">
+                    <h2 className="login-title">Authorization</h2>
+                        <label> Your email</label>
+                    <div className="input-login">
+                        <input type="email" minLength={5} maxLength={320} placeholder="example@example.com" />
+                        <img className="icon" src={require("../../assets/svg/email.png")}/>
+                    </div>
+                        <img className="line" src={require("../../assets/svg/line.png")} alt=""/>
+                        <label> Your password</label>
+                        <div className="input-login">
+                            <input type="password" minLength={6} maxLength={30} placeholder="*******" />
+                            <img className="icon" src={require("../../assets/svg/eye.png")}/>
+                            </div>
+                        <img className="line" src={require("../../assets/svg/line.png")} alt=""/>
+                    <button id="submit-authorization-form">Log in</button>
+                </form>
+            </div>
     )
 }
 
